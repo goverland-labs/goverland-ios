5626475315bb67526739aab92973cd46d83fa77f
Andrey Scherbovich <andrey.scherbovich@gmail.com>
2022-12-15 10:22:19 +0100

    bootstrap a new project from a default template

---
+++ DaoFeed/DaoFeed/DaoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaoFeedApp.swift
+//  DaoFeed
+//
+//  Created by Andrey Scherbovich on 15.12.22.
+//
+
+import SwiftUI
+
+@main
+struct DaoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
