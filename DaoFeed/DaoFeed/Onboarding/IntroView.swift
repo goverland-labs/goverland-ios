2bbfc40c8919648ab72130099786db5c32b97b57
Jenny Shalai <jenny.shalai@gmail.com>
2023-01-29 13:08:03 -0500

    added Intro model, Intros viewModel, splash screen, Intros screen as scrolling carousel, terms and privacy overlay

--- DaoFeed/DaoFeed/Onboarding/IntroView.swift
+++ DaoFeed/DaoFeed/Onboarding/IntroView.swift
@@ -7,22 +7,118 @@
 
 import SwiftUI
 
-struct IntroView: View {    
+struct IntroView: View {
+    
     @State var termsViewIsPresented = false
+    @State var intros = IntroViewModel.data.intros
+    @State var currentIndex: Int = 0
 
     var body: some View {
         VStack {
+            ScrollView(.init(), showsIndicators: false) {
+                CarouselView(intros: $intros, currentInde: $currentIndex)
+            }
+            .frame(height: 450)
+            .padding(.top, 100)
+            
             Spacer()
-            Text("Intro View")
-            Spacer()
-            Button("Start") {
+            Button("Get Started") {
                 termsViewIsPresented = true
             }
-        }.sheet(isPresented: $termsViewIsPresented) {
-            TermsView()
+            .frame(maxWidth: .infinity, maxHeight: 50, alignment: .center)
+            .background(Color.blue)
+            .clipShape(Capsule())
+            .tint(.white)
+            .fontWeight(.semibold)
+            .padding(.horizontal, 30)
+            .padding(.bottom, 50)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        .sheet(isPresented: $termsViewIsPresented) {
+            TermsView(termsViewIsPresented: $termsViewIsPresented)
                 .presentationDetents([.medium, .large])
         }
     }
+}
+
+extension View {
+    func getRect() -> CGRect {
+        return UIScreen.main.bounds
+    }
+}
+
+struct CarouselView: View {
+    
+    @Binding var intros: [IntroModel]
+    @Binding var currentInde: Int
+    @State var index: Int = 0
+    
+    var body: some View {
+        TabView(selection: $index) {
+            ForEach(intros.indices, id: \.self) { i in
+                VStack {
+                    Image(systemName: intros[i].image)
+                        .resizable()
+                        .frame(width: 120, height: 180, alignment: .center)
+                        .foregroundColor(.gray.opacity(0.6))
+                    Text(intros[i].title)
+                        .font(.title2)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 50)
+                    Text(intros[i].description)
+                        .padding(.top, 5)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 400)
+                .padding(.horizontal, 40)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct IntroModel: Identifiable {
+    let id: UUID
+    let title: String
+    let description: String
+    let image: String
+    
+    init(id: UUID, title: String, description: String, image: String) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.image = image
+    }
+}
+
+class IntroViewModel: ObservableObject {
+    
+    @Published var intros: [IntroModel] = []
+    static let data = IntroViewModel()
+    
+    private init() {
+        getIntros()
+    }
+    
+    func getIntros() {
+        let intro1 = IntroModel.init(id: UUID(),
+                                     title: "Follow important updates from your vorite DAOs",
+                                     description: "Receive push notifications",
+                                     image: "apps.iphone")
+        let intro2 = IntroModel.init(id: UUID(),
+                                     title: "Follow important updates from your vorite DAOs",
+                                     description: "Receive push notifications",
+                                     image: "building.columns")
+        let intro3 = IntroModel.init(id: UUID(),
+                                     title: "Follow important updates from your vorite DAOs",
+                                     description: "Receive push notifications",
+                                     image: "iphone.gen1")
+        intros.append(contentsOf: [intro1, intro2, intro3])
+    }
+    
 }
 
 struct IntroView_Previews: PreviewProvider {
--- DaoFeed/DaoFeed/Onboarding/TermsView.swift
+++ DaoFeed/DaoFeed/Onboarding/TermsView.swift
@@ -9,21 +9,76 @@ import SwiftUI
 
 struct TermsView: View {
     @Setting(\.termsAccepted) var termsAccepted
+    @Binding var termsViewIsPresented: Bool
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Here will be text with Terms")
-            Spacer()
-            Button("Accept") {
+        
+        VStack(spacing: 30) {
+           
+            HStack {
+                Spacer()
+                Button {
+                    termsViewIsPresented = false
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .symbolRenderingMode(.hierarchical)
+                        .foregroundColor(.gray)
+                        .frame(width: 30, height: 30)
+                }
+            }
+            
+            Text("Terms Of Use and Privacy Policy")
+                .fontWeight(.bold)
+                .font(.title3)
+            
+            VStack(alignment: .leading) {
+                HStack(alignment: .top, spacing: 10) {
+                    BulletedListsDot()
+                    Text("We collect anonymized app usage data and crash reports to ensure the quality of our app.")
+                }
+                
+                HStack(alignment: .top, spacing: 10) {
+                    BulletedListsDot()
+                    Text("We do not collect demographic data such as age or gander")
+                }
+                
+                HStack(alignment: .top, spacing: 10) {
+                    BulletedListsDot()
+                    Text("Read more in Privacy Policy and Terms")
+                }
+            }
+            
+            Button("Get Started") {
                 termsAccepted = true
             }
-        }.padding()
+            .frame(maxWidth: .infinity, maxHeight: 50, alignment: .center)
+            .background(Color.blue)
+            .clipShape(Capsule())
+            .tint(.white)
+            .fontWeight(.semibold)
+            .padding(.horizontal, 10)
+            
+            Button("Accept without sharing data") {
+                termsAccepted = true
+            }
+            .fontWeight(.medium)
+            
+        }.padding(.horizontal, 30)
+    }
+}
+
+fileprivate struct BulletedListsDot: View {
+    var body: some View {
+        Circle()
+            .fill(.black)
+            .frame(width: 8, height: 8)
+            .padding(.top, 6)
     }
 }
 
 struct TermsView_Previews: PreviewProvider {
     static var previews: some View {
-        TermsView()
+        TermsView(termsViewIsPresented: .constant(true))
     }
 }
