3b7d1f7b4f3cffa86143199fc93191ea662a4e06
Jenny Shalai <jenny.shalai@gmail.com>
2023-02-18 13:40:55 -0500

    added push notifications screen ui and user alert (static image for notifications)

--- Goverland/Goverland/Onboarding/EnablePushNotificationsView.swift
+++ Goverland/Goverland/Onboarding/EnablePushNotificationsView.swift
@@ -9,14 +9,50 @@ import SwiftUI
 
 struct EnablePushNotificationsView: View {
     @Setting(\.onboardingFinished) var onboardingFinished
-
+    
     var body: some View {
-        Spacer()
-        Text("Enable Push Notifications View")
-        Spacer()
-        Button("Finish onboarding") {
-            onboardingFinished = true
-        }.padding()
+        GeometryReader { geometry in
+            VStack {
+                Image("pushNotifications")
+                    .resizable()
+                    .aspectRatio(CGSize(width: 1548, height: 460), contentMode: .fit)
+                    .frame(width: geometry.size.width,
+                           height: geometry.size.height * 3 / 5,
+                           alignment: .center)
+                
+                VStack(spacing: 20) {
+                    Text("Never miss an update")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("Get push notifications about new proposales, votes, treasury movements, your delegates, activity, and more.")
+                        .multilineTextAlignment(.center)
+                        .lineSpacing(8)
+                        .padding(.bottom)
+                    
+                    Button("Enable notifications") {
+                        let center = UNUserNotificationCenter.current()
+                        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+                            if let error = error {
+                                // Handle the error here.
+                                print(error)
+                            }
+                            // Enable or disable features based on the authorization.
+                        }
+                    }
+                    .ghostActionButtonStyle()
+                    
+                    Button("No, thanks") {
+                        onboardingFinished = true
+                    }
+                    .fontWeight(.semibold)
+                    .padding(.bottom)
+                }
+                .frame(width: geometry.size.width * 0.9,
+                       height: geometry.size.height * 2 / 5,
+                       alignment: .center)
+            }
+        }
     }
 }
 
