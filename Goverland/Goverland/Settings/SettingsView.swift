56ea45559e2977bd1a1ed11e82091466d50cd741
Jenny Shalai <jenny.shalai@gmail.com>
2023-02-24 11:31:53 -0500

    added settings UI, added Advanced setting logic

--- Goverland/Goverland/Settings/SettingsView.swift
+++ Goverland/Goverland/Settings/SettingsView.swift
@@ -9,7 +9,48 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Settings View")
+        NavigationStack {
+            List {
+                Section {
+                    Text("Followed DAOs")
+                    Text("Notifications")
+                    Text("Appearance")
+                }
+                
+                Section(header: Text("Contact Us")) {
+                    Text("Twitter")
+                    Text("Telegram")
+                    Text("Email")
+                }
+                
+                Section {
+                    Text("About")
+                    Text("Help us grow")
+                    NavigationLink("Advanced") {
+                        AdvancedSettingView()
+                    }
+                    LabeledContent("App version", value: "0.1.0 (2)")
+                }
+            }
+        }
+    }
+}
+
+fileprivate struct AdvancedSettingView: View {
+    var body: some View {
+        VStack {
+            Text("Would you like to reset the App?")
+                .fontWeight(.bold)
+            Text("this action will delete your local settings and closer the application")
+                .padding(20)
+            Button("RESET") {
+                SettingKeys().onboardingFinished = false
+                SettingKeys().termsAccepted = false
+                exit(0)
+            }
+            .ghostActionButtonStyle()
+            
+        }
     }
 }
 
