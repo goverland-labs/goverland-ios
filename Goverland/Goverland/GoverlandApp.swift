3db3f236fd243f8c801d3b61bc755c0357f68a69
Jenny Shalai <jenny.shalai@gmail.com>
2023-03-15 10:12:21 -0400

    added Prod and Dev build configurations and custom flags DROD and DEV respectively

--- Goverland/Goverland/GoverlandApp.swift
+++ Goverland/Goverland/GoverlandApp.swift
@@ -10,6 +10,19 @@ import SwiftUI
 @main
 struct GoverlandApp: App {
     @StateObject var colorSchemeManager = ColorSchemeManager()
+    
+    init() {
+        #if DEV
+        print("DEV mode")
+        #elseif PROD
+        print("PROD mode")
+        #elseif DEBUG
+        print("DEBUG mode")
+        #else
+        print("RELEASE mode")
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
