87bb11faa84012573cc4f9ffc1cb5668b835ab8f
Jenny Shalai <jenny.shalai@gmail.com>
2023-03-23 10:43:15 -0400

    added global flag if the App tracks, added flag toggle in Settings

--- Goverland/Goverland/Analytics/ConsoleTracker.swift
+++ Goverland/Goverland/Analytics/ConsoleTracker.swift
@@ -6,24 +6,14 @@
 //
 
 import Foundation
-import LoggingKit
+import OSLog
 
 final class ConsoleTracker: TrackingHandler {
-    private var trackingEnabled = true
+    let logger = Logger()
 
-    /// You will find the events by the "[TRACKING]" tag in the log.
-    func track(event: String, parameters: [String: Any]?, file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
-        guard trackingEnabled else { return }
+    func track(event: String, parameters: [String: Any]?) {
+
         let parametersString = parameters != nil ? (", parameters: " + String(describing: parameters!)) : ""
-        LogService.shared.info("[TRACKING] event: '\(event)'" + parametersString, file: file, line: line, function: function)
-    }
-
-    func setUserProperty(_ value: String, file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
-        guard trackingEnabled else { return }
-        LogService.shared.info("[TRACKING] setUserProperty: '\(value)' for \(property.rawValue)", file: file, line: line, function: function)
-    }
-
-    func setTrackingEnabled(_ value: Bool) {
-        trackingEnabled = value
+        logger.log("[TRACKING] event: \(event) \(parametersString)")
     }
 }
