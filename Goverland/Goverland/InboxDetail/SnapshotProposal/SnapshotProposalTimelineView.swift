40675282f58bc9a36816a26af27ab8d2d39be4b1
Jenny Shalai <jenny.shalai@gmail.com>
2023-05-11 10:17:25 -0400

    added Timeline UI in snapshot proposal

--- Goverland/Goverland/InboxDetail/SnapshotProposal/SnapshotProposalTimelineView.swift
+++ Goverland/Goverland/InboxDetail/SnapshotProposal/SnapshotProposalTimelineView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct SnapshotProposalTimelineView: View {
+    private let testDates = [Date.now]
+    private let testEvents = ["Snapshot vote created by ", "Discussion started by "]
+    
     var body: some View {
-        Text("")
+        ForEach(0..<2) { i in
+            HStack(spacing: 2) {
+                //DateView(date: Date("Nov 5, 2022") ?? Date.now)
+                Text("Nov 5, 2022 - ")
+                Text(testEvents[i])
+                IdentityView(user: .test)
+            }
+            .font(.footnoteRegular)
+            .foregroundColor(.textWhite)
+            .minimumScaleFactor(0.8)
+            .lineLimit(1)
+        }
     }
 }
 
