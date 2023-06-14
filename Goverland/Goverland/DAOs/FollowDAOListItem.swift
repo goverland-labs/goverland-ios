5518a5d303579c3bad925d0a6d824fab4ae35c9b
Andrey Scherbovich <andrey@goverland.xyz>
2023-06-14 14:57:58 +0200

    try to fix git error

---
+++ Goverland/Goverland/DAOs/FollowDAOListItem.swift
@@ -0,0 +1,30 @@
+//
+//  FollowDaoListItemView.swift
+//  Goverland
+//
+//  Created by Andrey Scherbovich on 02.06.23.
+//
+
+import SwiftUI
+
+struct FollowDaoListItemView: View {
+    let dao: Dao
+
+    var body: some View {
+        HStack {
+            RoundPictureView(image: dao.image, imageSize: 50)
+            Text(dao.name)
+            Spacer()
+            FollowButtonView(isSubscribed: dao.subscriptionMeta != nil, daoID: dao.id)
+        }
+        .padding(5)
+        .listRowSeparator(.hidden)
+    }
+}
+
+struct FollowDAOListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowDaoListItemView(dao: .aave)
+    }
+}
+
