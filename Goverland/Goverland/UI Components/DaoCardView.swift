683d40850db31dd155b1b6c6e782c9087f98a12b
Andrey Scherbovich <andrey@goverland.xyz>
2023-06-17 14:32:03 +0200

    Gh 52 follow daos (#53)
    * small refactoring
    * fixing styling for DaoCardView
    * DaoListItemView style adjustment

---
+++ Goverland/Goverland/UI Components/DaoCardView.swift
@@ -0,0 +1,77 @@
+//
+//  DaoCardView.swift
+//  Goverland
+//
+//  Created by Jenny Shalai on 15.06.23.
+//
+
+import SwiftUI
+
+struct DaoCardView: View {
+    let dao: Dao
+
+    var body: some View {
+        VStack {
+            RoundPictureView(image: dao.image, imageSize: 90)
+                .padding(.top, 17)
+            
+            VStack(spacing: 3) {
+                Text(dao.name)
+                    .font(.headlineSemibold)
+                    .foregroundColor(.textWhite)
+                    .lineLimit(2)
+                    .multilineTextAlignment(.center)
+                Text("18.2K members")
+                    .font(.сaption2Regular)
+                    .foregroundColor(.textWhite60)
+            }
+            .padding(.horizontal, 12)
+
+            Spacer()
+            FollowButtonView(daoID: dao.id, subscriptionID: dao.subscriptionMeta?.id)
+                .padding(.bottom, 20)
+        }
+        .frame(width: 140, height: 215)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.container))
+    }
+}
+
+struct ShimmerDaoCardView: View {
+    var body: some View {
+        VStack {
+            ShimmerView()
+                .frame(width: 90, height: 90)
+                .cornerRadius(45)
+                .padding(.top, 17)
+
+            VStack(spacing: 3) {
+                ShimmerView()
+                    .frame(width: 60, height: 20)
+                    .cornerRadius(10)
+                ShimmerView()
+                    .frame(width: 80, height: 16)
+                    .cornerRadius(8)
+            }
+            .padding(.horizontal, 12)
+
+            Spacer()
+            ShimmerFollowButtonView()
+                .padding(.bottom, 20)
+        }
+        .frame(width: 140, height: 215)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.container))
+    }
+}
+
+struct DaoCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            DaoCardView(dao: .aave)
+            ShimmerDaoCardView()
+        }
+    }
+}
---
+++ Goverland/Goverland/UI Components/DaoListItemView.swift
@@ -0,0 +1,52 @@
+//
+//  FollowDaoListItemView.swift
+//  Goverland
+//
+//  Created by Andrey Scherbovich on 02.06.23.
+//
+
+import SwiftUI
+
+struct DaoListItemView: View {
+    let dao: Dao
+
+    var body: some View {
+        HStack {
+            RoundPictureView(image: dao.image, imageSize: 50)
+            Text(dao.name)
+                .font(.headlineSemibold)
+                .foregroundColor(.textWhite)
+            Spacer()
+            FollowButtonView(daoID: dao.id, subscriptionID: dao.subscriptionMeta?.id)
+        }
+        .padding(12)
+        .listRowSeparator(.hidden)
+    }
+}
+
+struct ShimmerDaoListItemView: View {
+    var body: some View {
+        HStack {
+            ShimmerView()
+                .frame(width: 50, height: 50)
+                .cornerRadius(25)
+            ShimmerView()
+                .frame(width: 150, height: 26)
+                .cornerRadius(13)
+            Spacer()
+            ShimmerFollowButtonView()
+        }
+        .padding(12)
+        .listRowSeparator(.hidden)
+    }
+}
+
+struct DapListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            DaoListItemView(dao: .aave)
+            ShimmerDaoListItemView()
+        }
+    }
+}
+
