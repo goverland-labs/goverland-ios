01e217436d721c38a467e70b339d4c005bc1a718
Jenny Shalai <jenny@goverland.xyz>
2023-06-28 10:10:02 -0400

    ready SnapshotProposalInfoView

--- Goverland/Goverland/Snapshot Proposal/Vote Tab/SnapshotProposalInfoView.swift
+++ Goverland/Goverland/Snapshot Proposal/Vote Tab/SnapshotProposalInfoView.swift
@@ -13,12 +13,9 @@ struct SnapshotProposalInfoView: View {
     var body: some View {
         HStack {
             VStack(alignment: .leading, spacing: 15) {
-                Text("Strategies")
-                Text("IPFS")
                 Text("Voting system")
                 Text("Start date")
                 Text("End date")
-                Text("Snapshot")
             }
             .font(.footnoteSemibold)
             .foregroundColor(.textWhite)
@@ -26,18 +23,9 @@ struct SnapshotProposalInfoView: View {
             Spacer()
             
             VStack(alignment: .trailing, spacing: 15) {
-                Text("strategies")
-                HStack(spacing: 0) {
-                    Text("#" + proposal.ipfs.prefix(7))
-                    Image(systemName: "arrow.up.forward.app")
-                }
                 Text(proposal.type.rawValue)
                 Text(proposal.votingStart.description)
                 Text(proposal.votingEnd.description)
-                HStack(spacing: 0) {
-                    Text(proposal.snapshot)
-                    Image(systemName: "arrow.up.forward.app")
-                }
             }
             .font(.footnoteSemibold)
             .foregroundColor(.textWhite)
