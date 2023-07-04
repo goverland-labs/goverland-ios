e17a2574bb177b14861655ca7390118c7f6baa53
Jenny Shalai <jenny@goverland.xyz>
2023-07-04 07:51:34 -0400

    added Snapshot RankedChoice Voting Result

--- Goverland/Goverland/Snapshot Proposal/Vote Tab/SnapshotRankedChoiceVotingResultView.swift
+++ Goverland/Goverland/Snapshot Proposal/Vote Tab/SnapshotRankedChoiceVotingResultView.swift
@@ -9,10 +9,64 @@ import SwiftUI
 
 struct SnapshotRankedChoiceVotingResultView: View {
     let proposal: Proposal
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ChoicesRankedChoiceResultView(choices: proposal.choices,
+                          scores: proposal.scores,
+                          scoresTotal: proposal.scoresTotal)
     }
 }
+
+struct ChoicesRankedChoiceResultView: View {
+    let choices: [String]
+    let scores: [Double]
+    let scoresTotal: Double
+
+    var body: some View {
+        VStack {
+            ForEach(choices.indices, id: \.self) { index in
+                let score = scores[index]
+                let totalScore = scoresTotal
+                SnapshotRankedChoiceVotingResultBarView(choice: choices[index],
+                                                        score: score,
+                                                        totalScore: totalScore)
+                .padding(.bottom, 30)
+            }
+        }
+    }
+}
+
+fileprivate struct SnapshotRankedChoiceVotingResultBarView: View {
+    let choice: String
+    let score: Double
+    let totalScore: Double
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 2) {
+                HStack {
+                    Text(choice)
+                        .font(.footnoteSemibold)
+                        .foregroundColor(.onSecondaryContainer)
+                    Spacer()
+                    Text(Utils.formattedNumber(score) + " | " + Utils.percentage(of: score, in: totalScore))
+                        .font(.footnoteSemibold)
+                        .foregroundColor(.textWhite)
+                }
+                
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .fill(Color.secondaryContainer)
+                        .frame(height: 6, alignment: .center)
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .fill(Color.primary)
+                        .frame(width: CGFloat(score/totalScore * 100) * CGFloat(geometry.size.width) / 100, height: 6, alignment: .center)
+                }
+            }
+        }
+    }
+}
+
 
 struct SnapshotRankedChoiceVotingResultView_Previews: PreviewProvider {
     static var previews: some View {
