f8d4994f32c0438b314dc3cc2ce37ea6eb02dc4d
Jenny Shalai <jenny@goverland.xyz>
2023-07-28 18:57:49 -0400

    added mock data for chart, added bar chart UI

--- Goverland/Goverland/DAOs/Info/DaoInfoInsightsDaoView.swift
+++ Goverland/Goverland/DAOs/Info/DaoInfoInsightsDaoView.swift
@@ -6,17 +6,65 @@
 //
 
 import SwiftUI
+import Charts
 
 struct DaoInfoInsightsDaoView: View {
     let dao: Dao
     
+    private let mockData = [
+        (votersType: "Existed Voters", data: ChampionVoters.oldChampionVoters),
+        (votersType: "New Voters", data: ChampionVoters.newChampionVoters)
+    ]
+    
     var body: some View {
-        VStack {
-            Text("Beautiful graphs are coming up!")
-            Spacer()
+        Chart {
+            ForEach(mockData, id: \.votersType) { element in
+                ForEach(element.data, id: \.date) {
+                    BarMark (
+                        x: .value("Date", $0.date),
+                        y: .value("Voters in K", $0.voters)
+                    )
+                }
+                .foregroundStyle(by: .value("Voters(type)", element.votersType))
+                
+            }
         }
+        .padding(.horizontal)
     }
 }
+
+struct ChampionVoters: Identifiable {
+    let id = UUID()
+    let date: String
+    let voters: Double
+}
+
+extension ChampionVoters {
+    static let oldChampionVoters: [ChampionVoters] = [
+        .init(date: "2023-01", voters: 11.4),
+        .init(date: "2023-02", voters: 10.9),
+        .init(date: "2023-03", voters: 9.9),
+        .init(date: "2023-04", voters: 10.4),
+        .init(date: "2023-05", voters: 11.4),
+        .init(date: "2023-06", voters: 10.9),
+        .init(date: "2023-07", voters: 9.9),
+        .init(date: "2023-08", voters: 10.4)
+    ]
+    
+    static let newChampionVoters: [ChampionVoters] = [
+        .init(date: "2023-01", voters: 2.3),
+        .init(date: "2023-02", voters: 1.1),
+        .init(date: "2023-03", voters: 2.9),
+        .init(date: "2023-04", voters: 2.4),
+        .init(date: "2023-05", voters: 2.3),
+        .init(date: "2023-06", voters: 1.1),
+        .init(date: "2023-07", voters: 2.9),
+        .init(date: "2023-08", voters: 2.4)
+    ]
+    
+}
+            
+        
 
 struct DaoInfoInsightsDaoView_Previews: PreviewProvider {
     static var previews: some View {
