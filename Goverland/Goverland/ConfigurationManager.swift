f9ffe0a3003c3e1efef2534118aeea88609ef104
Andrey Scherbovich <andrey@goverland.xyz>
2023-07-29 19:00:06 +0200

    Add configuration manager

---
+++ Goverland/Goverland/ConfigurationManager.swift
@@ -0,0 +1,46 @@
+//
+//  ConfigurationManager.swift
+//  Goverland
+//
+//  Created by Andrey Scherbovich on 29.07.23.
+//
+
+import Foundation
+
+class ConfigurationManager {
+    enum ConfigKeys: String {
+        case baseURL = "BASE_URL"
+        case defaultPaginationCount = "DEFAULT_PAGINATION_COUNT"
+        case timeout = "TIMEOUT"
+    }
+
+    static func value(for key: ConfigKeys) -> String {
+        if let value = Bundle.main.infoDictionary?[key.rawValue] as? String {
+            return value
+        }
+        fatalError("Invalid or missing Info.plist key: \(key.rawValue)")
+    }
+
+    // - MARK: Networking
+
+    static var baseURL: URL {
+        guard let url = URL(string: self.value(for: .baseURL)) else {
+            fatalError("Invalid Base URL")
+        }
+        return url
+    }
+
+    static var defaultPaginationCount: Int {
+        guard let count = Int(self.value(for: .defaultPaginationCount)) else {
+            fatalError("Invalid pagination count")
+        }
+        return count
+    }
+
+    static var timeout: TimeInterval {
+        guard let timeout = TimeInterval(self.value(for: .timeout)) else {
+            fatalError("Invalid timeout")
+        }
+        return timeout
+    }
+}
