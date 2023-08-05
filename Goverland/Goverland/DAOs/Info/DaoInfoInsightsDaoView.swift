0ab4d937a02b3cdd7c3648daf8d5aa7fe999b92c
Jenny Shalai <jenny@goverland.xyz>
2023-08-04 21:46:57 -0400

    WIP, added chart bar color

--- Goverland/Goverland/DAOs/Info/DaoInfoInsightsDaoView.swift
+++ Goverland/Goverland/DAOs/Info/DaoInfoInsightsDaoView.swift
@@ -10,61 +10,104 @@ import Charts
 
 struct DaoInfoInsightsDaoView: View {
     let dao: Dao
-    
-    private let mockData = [
-        (votersType: "Existed Voters", data: ChampionVoters.oldChampionVoters),
-        (votersType: "New Voters", data: ChampionVoters.newChampionVoters)
+    // for Chart bars animation
+    @State var mockChartData = [
+        (votersType: "Existed Voters", data: oldChampionVoters),
+        (votersType: "New Voters", data: newChampionVoters)
     ]
     
     var body: some View {
-        Chart {
-            ForEach(mockData, id: \.votersType) { element in
-                ForEach(element.data, id: \.date) {
-                    BarMark (
-                        x: .value("Date", $0.date),
-                        y: .value("Voters in K", $0.voters)
-                    )
+        VStack {
+            Chart {
+                ForEach(mockChartData, id: \.votersType) { element in
+                    ForEach(element.data, id: \.date) { data in
+                        BarMark (
+                            x: .value("Date", data.date, unit: .month),
+                            y: .value("Voters in K", data.voters)
+                        )
+                    }
+                    .foregroundStyle(by: .value("Voters(type)", element.votersType))
                 }
-                .foregroundStyle(by: .value("Voters(type)", element.votersType))
-                
             }
+            .frame(height: 200)
+            .padding(.horizontal)
+            
+            
+            Chart {
+                ForEach(mockChartData.first!.data) { element in
+                    BarMark (
+                        x: .value("Date", element.date),
+                        y: .value("Voters in K", element.voters)
+                    )
+                    .foregroundStyle(Color.chartBar)
+                }
+            }
+            .frame(height: 200)
+            .padding(.horizontal)
+            .onAppear() {
+                for (index, _) in mockChartData.first!.data.enumerated() {
+                    withAnimation(.easeInOut(duration: 0.8)) {
+                            mockChartData[0].data[index].animate = true
+                        }
+                }
+            }
+            
+            
+            
+            Spacer()
         }
-        .padding(.horizontal)
     }
 }
 
+
+
+
+
+
+
 struct ChampionVoters: Identifiable {
     let id = UUID()
-    let date: String
+    let date: Date
     let voters: Double
+    var animate: Bool = false
 }
 
-extension ChampionVoters {
-    static let oldChampionVoters: [ChampionVoters] = [
-        .init(date: "2023-01", voters: 11.4),
-        .init(date: "2023-02", voters: 10.9),
-        .init(date: "2023-03", voters: 9.9),
-        .init(date: "2023-04", voters: 10.4),
-        .init(date: "2023-05", voters: 11.4),
-        .init(date: "2023-06", voters: 10.9),
-        .init(date: "2023-07", voters: 9.9),
-        .init(date: "2023-08", voters: 10.4)
-    ]
-    
-    static let newChampionVoters: [ChampionVoters] = [
-        .init(date: "2023-01", voters: 2.3),
-        .init(date: "2023-02", voters: 1.1),
-        .init(date: "2023-03", voters: 2.9),
-        .init(date: "2023-04", voters: 2.4),
-        .init(date: "2023-05", voters: 2.3),
-        .init(date: "2023-06", voters: 1.1),
-        .init(date: "2023-07", voters: 2.9),
-        .init(date: "2023-08", voters: 2.4)
-    ]
-    
+private func formDate(year1: Int, month1: Int) -> Date {
+    Calendar.current.date(from: .init(year: year1, month: month1)) ?? Date()
 }
-            
-        
+
+
+    var oldChampionVoters: [ChampionVoters] = [
+        .init(date: formDate(year1: 2022, month1: 01), voters: 11.4),
+        .init(date: formDate(year1: 2022, month1: 02), voters: 10.9),
+        .init(date: formDate(year1: 2022, month1: 03), voters: 9.9),
+        .init(date: formDate(year1: 2022, month1: 04), voters: 10.4),
+        .init(date: formDate(year1: 2022, month1: 05), voters: 11.4),
+        .init(date: formDate(year1: 2022, month1: 06), voters: 10.9),
+        .init(date: formDate(year1: 2022, month1: 07), voters: 9.9),
+        .init(date: formDate(year1: 2022, month1: 08), voters: 10.4),
+        .init(date: formDate(year1: 2022, month1: 09), voters: 11.4),
+        .init(date: formDate(year1: 2022, month1: 10), voters: 10.9),
+        .init(date: formDate(year1: 2022, month1: 11), voters: 9.9),
+        .init(date: formDate(year1: 2022, month1: 12), voters: 10.4)
+    ]
+    
+    var newChampionVoters: [ChampionVoters] = [
+        .init(date: formDate(year1: 2022, month1: 01), voters: 2.3),
+        .init(date: formDate(year1: 2022, month1: 02), voters: 1.1),
+        .init(date: formDate(year1: 2022, month1: 03), voters: 2.9),
+        .init(date: formDate(year1: 2022, month1: 04), voters: 2.4),
+        .init(date: formDate(year1: 2022, month1: 05), voters: 2.3),
+        .init(date: formDate(year1: 2022, month1: 06), voters: 1.1),
+        .init(date: formDate(year1: 2022, month1: 07), voters: 2.9),
+        .init(date: formDate(year1: 2022, month1: 08), voters: 2.4),
+        .init(date: formDate(year1: 2022, month1: 09), voters: 2.3),
+        .init(date: formDate(year1: 2022, month1: 10), voters: 1.1),
+        .init(date: formDate(year1: 2022, month1: 11), voters: 2.9),
+        .init(date: formDate(year1: 2022, month1: 12), voters: 2.4)
+    ]
+
+
 
 struct DaoInfoInsightsDaoView_Previews: PreviewProvider {
     static var previews: some View {
