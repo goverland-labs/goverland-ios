00d0a3b63fe5a0f0bd0adb038eb0b221813b9d7c
Andrey Scherbovich <andrey@goverland.xyz>
2023-08-12 19:00:22 +0200

    Round top corners for Continue button background

--- Goverland/Goverland/Onboarding/OnboardingFollowDaosView.swift
+++ Goverland/Goverland/Onboarding/OnboardingFollowDaosView.swift
@@ -46,7 +46,11 @@ struct OnboardingFollowDaosView: View {
                                 path.append("EnablePushNotificationsView")
                             }
                             .padding()
-                            .background(Color(.systemBackground).opacity(0.8))
+                            .background(Color(.systemBackground)
+                                .opacity(0.8)
+                                .clipShape(TopRoundedCorner(radius: 20))
+                                .ignoresSafeArea()
+                            )
                             .navigationDestination(for: String.self) { _ in
                                 EnablePushNotificationsView()
                             }
@@ -89,8 +93,18 @@ struct OnboardingFollowDaosView: View {
     }
 }
 
-struct SelectDAOsView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingFollowDaosView()
+fileprivate struct TopRoundedCorner: Shape {
+    var radius: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + radius))
+        path.addQuadCurve(to: CGPoint(x: rect.minX + radius, y: rect.minY), control: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - radius, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.minY + radius), control: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+        return path
     }
 }
