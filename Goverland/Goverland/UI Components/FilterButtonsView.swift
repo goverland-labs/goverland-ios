7053e8a376897521b170faf539a3f80e0b93a123
Jenny Shalai <jenny@goverland.xyz>
2023-08-30 15:00:04 -0400

    fix

--- Goverland/Goverland/UI Components/FilterButtonsView.swift
+++ Goverland/Goverland/UI Components/FilterButtonsView.swift
@@ -48,3 +48,14 @@ struct FilterButtonsView<T: FilterOptions>: View {
         }
     }
 }
+
+fileprivate struct BottomBorder: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        
+        return path
+    }
+}
