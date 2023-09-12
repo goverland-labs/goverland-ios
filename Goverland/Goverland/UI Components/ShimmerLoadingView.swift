48f854e9244ef652454bac6b50eeea8a6d1ed507
Andrey Scherbovich <andrey@goverland.xyz>
2023-09-12 22:18:13 +0200

    Don't use shimmer view for RoundImageLoading using Kingfisher on iPads

--- Goverland/Goverland/UI Components/ShimmerLoadingView.swift
+++ Goverland/Goverland/UI Components/ShimmerLoadingView.swift
@@ -8,23 +8,37 @@
 import SwiftUI
 
 struct ShimmerView: View {
-    @State var startPoint: UnitPoint = .init(x: -1.8, y: -1.2)
-    @State var endPoint: UnitPoint = .init(x: 0, y: -0.2)
-    private var gradientColors = [
-        Color(uiColor: UIColor(.container)),
-        Color(uiColor: UIColor(.secondaryContainer)),
-        Color(uiColor: UIColor(.container))]
+    @State private var startPoint: UnitPoint
+    @State private var endPoint: UnitPoint
+    private var animate: Bool
+    private var gradientColors: [Color]       
+
+    init(startPoint: UnitPoint = .init(x: -1.8, y: -1.2),
+         endPoint: UnitPoint = .init(x: 0, y: -0.2),
+         animate: Bool = true,
+         gradientColors: [Color] = [
+            Color(uiColor: UIColor(.container)),
+            Color(uiColor: UIColor(.secondaryContainer)),
+            Color(uiColor: UIColor(.container))]
+    ) {
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+        self.animate = animate
+        self.gradientColors = gradientColors
+    }
     
     var body: some View {
-        LinearGradient (colors: gradientColors,
-                        startPoint: startPoint,
-                        endPoint: endPoint)
+        LinearGradient(colors: gradientColors,
+                       startPoint: startPoint,
+                       endPoint: endPoint)
         .onAppear {
-            withAnimation (.easeInOut (duration: 1)
-                .repeatCount(5, autoreverses: false)) {
-                    startPoint = .init(x: 1, y: 1)
-                    endPoint = .init(x: 2.2, y: 2.2)
-                }
+            if animate {
+                withAnimation (.easeInOut (duration: 1)
+                    .repeatCount(5, autoreverses: false)) {
+                        startPoint = .init(x: 1, y: 1)
+                        endPoint = .init(x: 2.2, y: 2.2)
+                    }
+            }
         }
     }
 }
