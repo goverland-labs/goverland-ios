9479cd57224e0163b9745e96c8ce9aea68836f7d
Andrey Scherbovich <andrey@goverland.xyz>
2023-09-13 15:09:01 +0200

    remove unused code

--- Goverland/Goverland/UI Components/ShimmerLoadingView.swift
+++ Goverland/Goverland/UI Components/ShimmerLoadingView.swift
@@ -10,12 +10,10 @@ import SwiftUI
 struct ShimmerView: View {
     @State private var startPoint: UnitPoint
     @State private var endPoint: UnitPoint
-    private var animate: Bool
-    private var gradientColors: [Color]       
+    private var gradientColors: [Color]
 
     init(startPoint: UnitPoint = .init(x: -1.8, y: -1.2),
          endPoint: UnitPoint = .init(x: 0, y: -0.2),
-         animate: Bool = true,
          gradientColors: [Color] = [
             Color(uiColor: UIColor(.container)),
             Color(uiColor: UIColor(.secondaryContainer)),
@@ -23,7 +21,6 @@ struct ShimmerView: View {
     ) {
         self.startPoint = startPoint
         self.endPoint = endPoint
-        self.animate = animate
         self.gradientColors = gradientColors
     }
     
@@ -32,13 +29,11 @@ struct ShimmerView: View {
                        startPoint: startPoint,
                        endPoint: endPoint)
         .onAppear {
-            if animate {
-                withAnimation (.easeInOut (duration: 1)
-                    .repeatCount(5, autoreverses: false)) {
-                        startPoint = .init(x: 1, y: 1)
-                        endPoint = .init(x: 2.2, y: 2.2)
-                    }
-            }
+            withAnimation (.easeInOut (duration: 1)
+                .repeatCount(5, autoreverses: false)) {
+                    startPoint = .init(x: 1, y: 1)
+                    endPoint = .init(x: 2.2, y: 2.2)
+                }            
         }
     }
 }
