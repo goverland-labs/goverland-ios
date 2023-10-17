b9bc4bdfff6dd2330bc6244dba043b573dc43906
Andrey Scherbovich <andrey@goverland.xyz>
2023-10-17 13:08:52 +0200

    Add animated envelope icon

--- Goverland/Goverland/Home Tab/Dashboard/DashboardView.swift
+++ Goverland/Goverland/Home Tab/Dashboard/DashboardView.swift
@@ -14,6 +14,8 @@ fileprivate enum Path {
 struct DashboardView: View {
     @State private var path = NavigationPath()
     @StateObject private var dataSource = DashboardViewDataSource.shared
+    @Setting(\.unreadEvents) var unreadEvents
+    @State private var animate = false
 
     var body: some View {
         NavigationStack(path: $path) {
@@ -24,17 +26,28 @@ struct DashboardView: View {
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     VStack {
-                        Text("Dashboard")
+                        Text("Goverland")
                             .font(.title3Semibold)
                             .foregroundColor(Color.textWhite)
                     }
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    Image(systemName: "envelope")
-                        .foregroundColor(.textWhite)
-                        .onTapGesture {
-                            ActiveHomeViewManager.shared.activeView = .inbox
-                        }
+                    if #available(iOS 17, *) {
+                        Image(systemName: "envelope.badge.fill")
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(unreadEvents > 0 ? Color.primary : Color.textWhite, Color.textWhite)
+                            .symbolEffect(.bounce.up.byLayer, options: .speed(0.3), value: animate)
+                            .onTapGesture {
+                                ActiveHomeViewManager.shared.activeView = .inbox
+                            }
+                    } else {
+                        Image(systemName: "envelope.badge.fill")
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(unreadEvents > 0 ? Color.primary : Color.textWhite, Color.textWhite)
+                            .onTapGesture {
+                                ActiveHomeViewManager.shared.activeView = .inbox
+                            }
+                    }
                 }
             }
             .navigationDestination(for: Path.self) { path in
@@ -43,6 +56,7 @@ struct DashboardView: View {
                 }
             }
             .onAppear {
+                animate.toggle()
                 // TODO: track
             }
         }
