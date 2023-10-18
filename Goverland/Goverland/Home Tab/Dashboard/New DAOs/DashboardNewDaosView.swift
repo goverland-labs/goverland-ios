8799bd000fa8ff8bc144bad15edc8861e783ef35
Andrey Scherbovich <andrey@goverland.xyz>
2023-10-18 21:10:38 +0200

    Add refresh icon

--- Goverland/Goverland/Home Tab/Dashboard/New DAOs/DashboardNewDaosView.swift
+++ Goverland/Goverland/Home Tab/Dashboard/New DAOs/DashboardNewDaosView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct DashboardNewDaosView: View {
     @EnvironmentObject private var activeSheetManger: ActiveSheetManager
+    @ObservedObject var dataSource = GroupedDaosDataSource.dashboard
 
     var body: some View {
-        DaoThreadForCategoryView(dataSource: GroupedDaosDataSource.dashboard,
-                                 category: DaoCategory.new,
-                                 onSelectDao: { dao in activeSheetManger.activeSheet = .daoInfo(dao); Tracker.track(.dashNewDaoOpen) },
-                                 onFollowToggle: { if $0 { Tracker.track(.dashNewDaoFollow) } })
+        if dataSource.failedToLoadInitialData {
+            RefreshIcon {
+                dataSource.refresh()
+            }
+        } else {
+            DaoThreadForCategoryView(dataSource: dataSource,
+                                     category: DaoCategory.new,
+                                     onSelectDao: { dao in activeSheetManger.activeSheet = .daoInfo(dao); Tracker.track(.dashNewDaoOpen) },
+                                     onFollowToggle: { if $0 { Tracker.track(.dashNewDaoFollow) } })
             .padding(.leading, 8)
             .padding(.bottom, 16)
+        }
     }
 }
