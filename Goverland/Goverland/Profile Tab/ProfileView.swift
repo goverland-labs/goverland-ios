16a70f519dfbc49ae346d9ef7a21985fbf65be58
Andrey Scherbovich <andrey@goverland.xyz>
2023-11-11 11:25:17 +0100

    Profile improvement

--- Goverland/Goverland/Profile Tab/ProfileView.swift
+++ Goverland/Goverland/Profile Tab/ProfileView.swift
@@ -25,124 +25,18 @@ struct ProfileView: View {
     @Setting(\.authToken) var authToken
 
     var body: some View {
-        if authToken.isEmpty {
-            SignInView()
-        } else {
-            _ProfileView(path: $path)
-        }
-    }
-}
-
-fileprivate struct _ProfileView: View {
-    @Binding var path: [ProfileScreen]
-
-    @State private var isDeleteProfilePopoverPresented = false
-    @State private var isSignOutPopoverPresented = false
-
-    private let user = User.aaveChan
-    private let devices = [["IPhone 14 Pro", "Sandefjord, Norway", "online"],
-                           ["IPhone 13", "Tbilisi, Georgia", "Apr 10"]]
-
-    var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                Circle()
-                    .fill(Color.gray)
-                    .frame(width: 70, height: 70)
-                Text((user.resolvedName != nil) ? user.resolvedName! : "Unnamed")
-                    .font(.title3Semibold)
-                    .foregroundColor(.textWhite)
-            }
-            List {
-                Section {
-                    NavigationLink("My followed DAOs", value: ProfileScreen.subscriptions)
-                }
-
-                Section() {
-                    NavigationLink("", destination: EmptyView())
-                        .background(
-                            HStack {
-                                Text("Accounts")
-                                Spacer()
-                                Image(systemName: "plus")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 17)
-                                    .foregroundColor(Color.onPrimary)
-                            }
-                                .foregroundColor(Color.onPrimary)
-                        )
-                        .listRowBackground(Color.primaryDim)
-
-                    if user.resolvedName != nil {
-                        NavigationLink("", destination: EmptyView())
-                            .frame(height: 40)
-                            .background(
-                                HStack {
-                                    Image(systemName: "circle")
-                                        .frame(width:20, height: 20)
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text(user.resolvedName!)
-                                            .font(.bodyRegular)
-                                            .foregroundColor(.textWhite)
-                                        Text(user.address.short)
-                                            .font(.сaptionRegular)
-                                            .foregroundColor(.textWhite60)
-                                    }
-                                    Spacer()
-                                })
-                    }
-
-                    NavigationLink("", destination: EmptyView())
-                        .background(
-                            HStack {
-                                HStack {
-                                    Image(systemName: "circle")
-                                        .frame(width:20, height: 20)
-                                    Text(user.address.short)
-                                        .font(.bodyRegular)
-                                        .foregroundColor(.textWhite)
-                                }
-                                Spacer()
-                            })
-                }
-
-                Section(header: Text("Devices")) {
-                    ForEach(devices.indices) { i in
-                        NavigationLink("", destination: EmptyView())
-                            .frame(height: 40)
-                            .background(
-                                HStack {
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text(devices[i][0])
-                                            .font(.bodyRegular)
-                                            .foregroundColor(.textWhite)
-                                        Text("\(devices[i][1]) - \(devices[i][2])")
-                                            .font(.footnoteRegular)
-                                            .foregroundColor(.textWhite60)
-                                    }
-                                    Spacer()
-                                }
-                            )}
-                }
-
-                Section() {
-                    Button("Sign out") {
-                        isSignOutPopoverPresented.toggle()
-                    }
-                    .tint(Color.textWhite)
+            VStack(spacing: 0) {
+                if authToken.isEmpty {
+                    SignInView()
+                } else {
+                    _ProfileView()
                 }
             }
+
+            .scrollIndicators(.hidden)
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        path.append(.settings)
-                    } label: {
-                        Image("settings-active")
-                    }
-                }
-
                 ToolbarItem(placement: .principal) {
                     VStack {
                         Text("Profile")
@@ -151,18 +45,11 @@ fileprivate struct _ProfileView: View {
                     }
                 }
 
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    Menu {
-                        Button {
-                            isDeleteProfilePopoverPresented.toggle()
-                        } label: {
-                            Label("Delete Profile", systemImage: "trash.fill")
-                        }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        path.append(.settings)
                     } label: {
-                        Image(systemName: "ellipsis")
-                            .foregroundColor(.primary)
-                            .fontWeight(.bold)
-                            .frame(height: 20)
+                        Image(systemName: "gearshape")
                     }
                 }
             }
@@ -171,7 +58,7 @@ fileprivate struct _ProfileView: View {
                 case .settings: SettingsView()
                 case .subscriptions: SubscriptionsView()
 
-                // Settings
+                    // Settings
                 case .pushNofitications: PushNotificationsSettingView()
                 case .about: AboutSettingView()
                 case .helpUsGrow: HelpUsGrowSettingView()
@@ -179,14 +66,119 @@ fileprivate struct _ProfileView: View {
                 case .advanced: AdvancedSettingView()
                 }
             }
-            .sheet(isPresented: $isDeleteProfilePopoverPresented) {
-                DeleteProfilePopoverView()
-                    .presentationDetents([.medium, .large])
+        }
+    }
+}
+
+fileprivate struct _ProfileView: View {
+    @State private var isDeleteProfilePopoverPresented = false
+    @State private var isSignOutPopoverPresented = false
+
+    private let user = User.aaveChan
+    private let devices = [["IPhone 14 Pro", "Sandefjord, Norway", "online"],
+                           ["IPhone 13", "Tbilisi, Georgia", "Apr 10"]]
+
+    var body: some View {
+        VStack {
+            Circle()
+                .fill(Color.gray)
+                .frame(width: 70, height: 70)
+            Text((user.resolvedName != nil) ? user.resolvedName! : "Unnamed")
+                .font(.title3Semibold)
+                .foregroundColor(.textWhite)
+        }
+        List {
+            Section {
+                NavigationLink("My followed DAOs", value: ProfileScreen.subscriptions)
             }
-            .popover(isPresented: $isSignOutPopoverPresented) {
-                SignOutPopoverView()
-                    .presentationDetents([.fraction(0.15)])
+
+            Section() {
+                NavigationLink("", destination: EmptyView())
+                    .background(
+                        HStack {
+                            Text("Accounts")
+                            Spacer()
+                            Image(systemName: "plus")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 17)
+                                .foregroundColor(Color.onPrimary)
+                        }
+                            .foregroundColor(Color.onPrimary)
+                    )
+                    .listRowBackground(Color.primaryDim)
+
+                if user.resolvedName != nil {
+                    NavigationLink("", destination: EmptyView())
+                        .frame(height: 40)
+                        .background(
+                            HStack {
+                                Image(systemName: "circle")
+                                    .frame(width:20, height: 20)
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text(user.resolvedName!)
+                                        .font(.bodyRegular)
+                                        .foregroundColor(.textWhite)
+                                    Text(user.address.short)
+                                        .font(.сaptionRegular)
+                                        .foregroundColor(.textWhite60)
+                                }
+                                Spacer()
+                            })
+                }
+
+                NavigationLink("", destination: EmptyView())
+                    .background(
+                        HStack {
+                            HStack {
+                                Image(systemName: "circle")
+                                    .frame(width:20, height: 20)
+                                Text(user.address.short)
+                                    .font(.bodyRegular)
+                                    .foregroundColor(.textWhite)
+                            }
+                            Spacer()
+                        })
             }
+
+            Section(header: Text("Devices")) {
+                ForEach(devices.indices) { i in
+                    NavigationLink("", destination: EmptyView())
+                        .frame(height: 40)
+                        .background(
+                            HStack {
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text(devices[i][0])
+                                        .font(.bodyRegular)
+                                        .foregroundColor(.textWhite)
+                                    Text("\(devices[i][1]) - \(devices[i][2])")
+                                        .font(.footnoteRegular)
+                                        .foregroundColor(.textWhite60)
+                                }
+                                Spacer()
+                            }
+                        )}
+            }
+
+            Section() {
+                Button("Sign out") {
+                    isSignOutPopoverPresented.toggle()
+                }
+                .tint(Color.textWhite)
+
+                Button("Delete profile") {
+                    isDeleteProfilePopoverPresented.toggle()
+                }
+                .tint(Color.textWhite)
+            }
+        }
+        .sheet(isPresented: $isDeleteProfilePopoverPresented) {
+            DeleteProfilePopoverView()
+                .presentationDetents([.medium, .large])
+        }
+        .popover(isPresented: $isSignOutPopoverPresented) {
+            SignOutPopoverView()
+                .presentationDetents([.fraction(0.15)])
         }
     }
 }
