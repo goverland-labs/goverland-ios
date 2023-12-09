5308c3eb4f7cdfcab426e3b4a578e8463902853b
Andrey Scherbovich <andrey@goverland.xyz>
2023-12-09 15:53:33 +0100

    WIP:  Cast your vote beautifying

--- Goverland/Goverland/Snapshot Proposal/Vote Tab/Cast your vote/CastYourVoteView.swift
+++ Goverland/Goverland/Snapshot Proposal/Vote Tab/Cast your vote/CastYourVoteView.swift
@@ -32,12 +32,7 @@ fileprivate struct _VoteView: View {
     @Environment(\.dismiss) private var dismiss
     @FocusState private var isTextEditorFocused: Bool
 
-    private var vpSymbol: String {
-        if let symbol = model.proposal.symbol, !symbol.isEmpty {
-            return symbol
-        }
-        return "VOTE"
-    }
+    let bottomElementId = 100
 
     private var user: User {
         let profile = profiles.first(where: { $0.selected })!
@@ -47,126 +42,103 @@ fileprivate struct _VoteView: View {
     }
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 8) {
-                Text("Cast your vote")
-                    .foregroundColor(.textWhite)
-                    .font(.title3Semibold)
+        ZStack {
+            ScrollViewReader { proxy in
+                ScrollView {
+                    VStack(spacing: 8) {
+                        _HeaderView(model: model, user: user)
 
-                Image("cast-your-vote")
-                    .padding(.vertical, 32)
-                    .frame(width: 186)
-                    .scaledToFit()
+                        if (model.validated ?? false) && !model.isShieldedVoting {
+                            ZStack(alignment: .topLeading) {
+                                TextEditor(text: $model.reason)
+                                    .focused($isTextEditorFocused)
+                                    .frame(height: 96)
+                                    .foregroundColor(.textWhite)
+                                    .accentColor(.textWhite40)
+                                    .scrollContentBackground(.hidden)
+                                    .background(Color.containerBright)
+                                    .cornerRadius(20)
 
-                HStack {
-                    Text("Account")
-                        .foregroundColor(.textWhite)
-                    Spacer()
-                    IdentityView(user: user)
-                }
+                                // TextEditor doesn't have a placeholder support yet
+                                if !isTextEditorFocused && model.reason.isEmpty {
+                                    Text("Share your reason (optional)")
+                                        .allowsHitTesting(false)
+                                        .foregroundColor(.textWhite20)
+                                        .padding(16)
+                                }
+                            }
+                            .padding(.top, 16)
+                        }
 
-                HStack {
-                    Text("Voting power")
-                        .foregroundColor(.textWhite)
-                    Spacer()
-                    Text("\(model.votingPower) \(vpSymbol)" )
-                        .foregroundColor(.textWhite)
-                }
+                        if let errorMessage = model.errorMessage {
+                            VStack(spacing: 0) {
+                                HStack(spacing: 8) {
+                                    Image(systemName: "exclamationmark.triangle.fill")
+                                        .foregroundColor(.dangerText)
+                                    Text(errorMessage)
+                                        .font(.bodyRegular)
+                                        .foregroundColor(.textWhite)
+                                }
+                                .padding(.horizontal, 8)
+                                .padding(.vertical, 16)
+                            }
+                            .background {
+                                RoundedRectangle(cornerRadius: 13)
+                                    .fill(Color.containerBright)
+                            }
+                            .padding(.top, 24)
+                        }
 
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.secondaryContainer)
-                    .frame(height: 2)
+                        Spacer()
 
-                HStack {
-                    Text("Choice")
-                        .foregroundColor(.textWhite)
-                    Spacer()
-                    Text(model.choiceStr)
-                        .foregroundColor(.textWhite)
-                }
-
-                HStack {
-                    Text("Validation")
-                    Spacer()
-
-                    if model.failedToValidate {
-                        Text("-")
-                            .foregroundColor(.textWhite)
-                    } else if model.validated == nil { // validation in progress
-                        ProgressView()
-                            .foregroundColor(.textWhite20)
-                            .controlSize(.mini)
-                    } else if model.validated! {
-                        Image(systemName: "checkmark")
-                            .foregroundColor(.primaryDim)
-                    } else {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.dangerText)
+                        Group {
+                            if let message = model.infoMessage {
+                                VStack(spacing: 0) {
+                                    HStack(spacing: 8) {
+                                        Image(systemName: "info.circle.fill")
+                                            .foregroundColor(.textWhite)
+                                        Text(message)
+                                            .font(.bodyRegular)
+                                            .foregroundColor(.textWhite)
+                                    }
+                                    .padding(.horizontal, 8)
+                                    .padding(.vertical, 16)
+                                }
+                                .background {
+                                    RoundedRectangle(cornerRadius: 13)
+                                        .fill(Color.containerBright)
+                                }
+                            } else {
+                                Spacer()
+                            }
+                        }
+                        .id(bottomElementId)
+                        .padding(.bottom, isTextEditorFocused ? 50 : 16)
                     }
                 }
-
-                if (model.validated ?? false) && !model.isShieldedVoting {
-                    ZStack(alignment: .topLeading) {
-                        TextEditor(text: $model.reason)
-                            .focused($isTextEditorFocused)
-                            .frame(height: 96)
-                            .foregroundColor(.textWhite)
-                            .accentColor(.textWhite40)
-                            .scrollContentBackground(.hidden)
-                            .background(Color.containerBright)
-                            .cornerRadius(20)
-
-                        // TextEditor doesn't have a placeholder support yet
-                        if !isTextEditorFocused && model.reason.isEmpty {
-                            Text("Share your reason (optional)")
-                                .allowsHitTesting(false)
-                                .foregroundColor(.textWhite20)
-                                .padding(16)
+                .scrollIndicators(.hidden)
+                .onTapGesture {
+                    // To hide keyboard when tapping outside of TextEditor
+                    isTextEditorFocused = false
+                }
+                .onChange(of: isTextEditorFocused) {
+                    if isTextEditorFocused {
+                        // Initially system scrolls to the TextEditor, then we scroll
+                        // to the bottom Group element (into message or spacer) to make
+                        // buttons visible and not layered on top of other elements.
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            withAnimation {
+                                proxy.scrollTo(bottomElementId)
+                            }
                         }
                     }
-                    .padding(.top, 16)
                 }
+            }
 
-                if let errorMessage = model.errorMessage {
-                    VStack(spacing: 0) {
-                        HStack(spacing: 8) {
-                            Image(systemName: "exclamationmark.triangle.fill")
-                                .foregroundColor(.dangerText)
-                            Text(errorMessage)
-                                .font(.bodyRegular)
-                                .foregroundColor(.textWhite)
-                        }
-                        .padding(.horizontal, 8)
-                        .padding(.vertical, 16)
-                    }
-                    .background {
-                        RoundedRectangle(cornerRadius: 13)
-                            .fill(Color.containerBright)
-                    }
-                    .padding(.top, 24)
-                }
 
+            // Attach buttons to the bottom of the screen
+            VStack {
                 Spacer()
-
-                if let message = model.infoMessage {
-                    VStack(spacing: 0) {
-                        HStack(spacing: 8) {
-                            Image(systemName: "info.circle.fill")
-                                .foregroundColor(.textWhite)
-                            Text(message)
-                                .font(.bodyRegular)
-                                .foregroundColor(.textWhite)
-                        }
-                        .padding(.horizontal, 8)
-                        .padding(.vertical, 16)
-                    }
-                    .background {
-                        RoundedRectangle(cornerRadius: 13)
-                            .fill(Color.containerBright)
-                    }
-                    .padding(.bottom, 16)
-                }
-
                 HStack(spacing: 16) {
                     SecondaryButton("Cancel") {
                         dismiss()
@@ -176,19 +148,86 @@ fileprivate struct _VoteView: View {
                         model.prepareVote(address: user.address.value)
                     }
                 }
-                .padding(.horizontal)
             }
         }
-        .scrollIndicators(.hidden)
-        .onTapGesture {
-            // To hide keyboard when tapping outside of TextEditor
-            isTextEditorFocused = false
-        }
-        .padding(.horizontal, 16)
-        .padding(.vertical, 16)
         .onAppear {
             // TODO: track
             model.validate(address: user.address.value)
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 16)
+
+    }
+}
+
+fileprivate struct _HeaderView: View {
+    @StateObject var model: CastYourVoteDataSource
+    let user: User
+
+    private var vpSymbol: String {
+        if let symbol = model.proposal.symbol, !symbol.isEmpty {
+            return symbol
+        }
+        return "VOTE"
+    }
+
+    var body: some View {
+        VStack {
+            Text("Cast your vote")
+                .foregroundColor(.textWhite)
+                .font(.title3Semibold)
+
+            Image("cast-your-vote")
+                .padding(.vertical, 32)
+                .frame(width: 186)
+                .scaledToFit()
+
+            HStack {
+                Text("Account")
+                    .foregroundColor(.textWhite)
+                Spacer()
+                IdentityView(user: user)
+            }
+
+            HStack {
+                Text("Voting power")
+                    .foregroundColor(.textWhite)
+                Spacer()
+                Text("\(model.votingPower) \(vpSymbol)" )
+                    .foregroundColor(.textWhite)
+            }
+
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.secondaryContainer)
+                .frame(height: 2)
+
+            HStack {
+                Text("Choice")
+                    .foregroundColor(.textWhite)
+                Spacer()
+                Text(model.choiceStr)
+                    .foregroundColor(.textWhite)
+            }
+
+            HStack {
+                Text("Validation")
+                Spacer()
+
+                if model.failedToValidate {
+                    Text("-")
+                        .foregroundColor(.textWhite)
+                } else if model.validated == nil { // validation in progress
+                    ProgressView()
+                        .foregroundColor(.textWhite20)
+                        .controlSize(.mini)
+                } else if model.validated! {
+                    Image(systemName: "checkmark")
+                        .foregroundColor(.primaryDim)
+                } else {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.dangerText)
+                }
+            }
         }
     }
 }
