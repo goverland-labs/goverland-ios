279a66425415ea3a67c85407eb56c201b7b8500f
Andrey Scherbovich <andrey@goverland.xyz>
2023-12-09 19:17:49 +0100

    small refactoring

--- Goverland/Goverland/Snapshot Proposal/Vote Tab/Cast your vote/CastYourVoteView.swift
+++ Goverland/Goverland/Snapshot Proposal/Vote Tab/Cast your vote/CastYourVoteView.swift
@@ -32,7 +32,7 @@ fileprivate struct _VoteView: View {
     @Environment(\.dismiss) private var dismiss
     @FocusState private var isTextEditorFocused: Bool
 
-    let bottomElementId = 100
+    let bottomElementId = UUID()
 
     private var user: User {
         let profile = profiles.first(where: { $0.selected })!
@@ -47,6 +47,8 @@ fileprivate struct _VoteView: View {
                 ScrollView {
                     VStack(spacing: 8) {
                         _HeaderView(model: model, user: user)
+
+                        // Reason Text Input
 
                         if (model.validated ?? false) && !model.isShieldedVoting {
                             ZStack(alignment: .topLeading) {
@@ -70,50 +72,25 @@ fileprivate struct _VoteView: View {
                             .padding(.top, 16)
                         }
 
+                        // Error message
+
                         if let errorMessage = model.errorMessage {
-                            VStack(spacing: 0) {
-                                HStack(spacing: 8) {
-                                    Image(systemName: "exclamationmark.triangle.fill")
-                                        .foregroundColor(.dangerText)
-                                    Text(errorMessage)
-                                        .font(.bodyRegular)
-                                        .foregroundColor(.textWhite)
-                                }
-                                .padding(.horizontal, 8)
-                                .padding(.vertical, 16)
-                            }
-                            .background {
-                                RoundedRectangle(cornerRadius: 13)
-                                    .fill(Color.containerBright)
-                            }
-                            .padding(.top, 24)
+                            _ErrorMessageView(message: errorMessage)
                         }
 
                         Spacer()
 
+                        // Info message
+
                         Group {
                             if let message = model.infoMessage {
-                                VStack(spacing: 0) {
-                                    HStack(spacing: 8) {
-                                        Image(systemName: "info.circle.fill")
-                                            .foregroundColor(.textWhite)
-                                        Text(message)
-                                            .font(.bodyRegular)
-                                            .foregroundColor(.textWhite)
-                                    }
-                                    .padding(.horizontal, 8)
-                                    .padding(.vertical, 16)
-                                }
-                                .background {
-                                    RoundedRectangle(cornerRadius: 13)
-                                        .fill(Color.containerBright)
-                                }
+                                _InfoMessageView(message: message)
                             } else {
                                 Spacer()
                             }
                         }
                         .id(bottomElementId)
-                        .padding(.bottom, isTextEditorFocused ? 50 : 16)
+                        .padding(.bottom, 50)
                     }
                 }
                 .scrollIndicators(.hidden)
@@ -123,10 +100,9 @@ fileprivate struct _VoteView: View {
                 }
                 .onChange(of: isTextEditorFocused) {
                     if isTextEditorFocused {
-                        // Initially system scrolls to the TextEditor, then we scroll
-                        // to the bottom Group element (into message or spacer) to make
-                        // buttons visible and not layered on top of other elements.
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        // Initially the system scrolls to the TextEditor, then we scroll to the bottom `Group`
+                        // element to make buttons visible and not layered on top of other elements.
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
                             withAnimation {
                                 proxy.scrollTo(bottomElementId)
                             }
@@ -135,19 +111,23 @@ fileprivate struct _VoteView: View {
                 }
             }
 
-
             // Attach buttons to the bottom of the screen
             VStack {
                 Spacer()
-                HStack(spacing: 16) {
-                    SecondaryButton("Cancel") {
-                        dismiss()
+                VStack {
+                    HStack(spacing: 16) {
+                        SecondaryButton("Cancel") {
+                            dismiss()
+                        }
+                        PrimaryButton("Sign",
+                                      isEnabled: (model.validated ?? false) && !model.isPreparing && !model.isSubmitting) {
+                            model.prepareVote(address: user.address.value)
+                            isTextEditorFocused = false
+                        }
                     }
-                    PrimaryButton("Sign",
-                                  isEnabled: (model.validated ?? false) && !model.isPreparing && !model.isSubmitting) {
-                        model.prepareVote(address: user.address.value)
-                    }
+                    .padding(.top, 16)
                 }
+                .background(Color(UIColor.systemBackground))
             }
         }
         .onAppear {
@@ -228,6 +208,51 @@ fileprivate struct _HeaderView: View {
                         .foregroundColor(.dangerText)
                 }
             }
+        }
+    }
+}
+
+fileprivate struct _ErrorMessageView: View {
+    let message: String
+
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(spacing: 8) {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .foregroundColor(.dangerText)
+                Text(message)
+                    .font(.bodyRegular)
+                    .foregroundColor(.textWhite)
+            }
+            .padding(.horizontal, 8)
+            .padding(.vertical, 16)
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 13)
+                .fill(Color.containerBright)
+        }
+        .padding(.top, 24)
+    }
+}
+
+fileprivate struct _InfoMessageView: View {
+    let message: String
+
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(spacing: 8) {
+                Image(systemName: "info.circle.fill")
+                    .foregroundColor(.textWhite)
+                Text(message)
+                    .font(.bodyRegular)
+                    .foregroundColor(.textWhite)
+            }
+            .padding(.horizontal, 8)
+            .padding(.vertical, 16)
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 13)
+                .fill(Color.containerBright)
         }
     }
 }
