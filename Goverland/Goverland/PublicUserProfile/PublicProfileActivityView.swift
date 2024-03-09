345c0de2c57b229e2d420aa60383415b86125435
Jenny Shalai <jenny.shalai@gmail.com>
2024-03-09 08:39:08 -0500

    VotesListView UI

--- Goverland/Goverland/PublicUserProfile/PublicProfileActivityView.swift
+++ Goverland/Goverland/PublicUserProfile/PublicProfileActivityView.swift
@@ -51,12 +51,52 @@ fileprivate struct VotesInDaosView: View {
 fileprivate struct VotesListView: View {
     let data: PublicProfileDataSource
     var body: some View {
-        if data.failedToLoadIVotesData {
-            RefreshIcon {
+        VStack {
+            HStack {
+                Text("My votes (\(data.total ?? 0))")
+                    .font(.subheadlineSemibold)
+                    .foregroundStyle(Color.textWhite)
+                Spacer()
+                NavigationLink("See all", value: ProfileScreen.votes)
+                    .font(.subheadlineSemibold)
+                    .foregroundStyle(Color.primaryDim)
+            }
+            .padding(.top, 16)
+            .padding(.horizontal, 16)
+
+            if data.failedToLoadInitialData {
+                RefreshIcon {
+                    data.getVotes()
+                }
+            } else if data.isLoading && data.votes == nil {
+                ForEach(0..<3) { _ in
+                    ShimmerProposalListItemCondensedView()
+                        .padding(.horizontal, 8)
+                }
+            } else if data.votes?.isEmpty ?? false {
+                Text("You have not voted yet")
+                    .foregroundStyle(Color.textWhite)
+                    .font(.bodyRegular)
+                    .padding(16)
+            } else {
+                ForEach(data.votes.prefix(3)) { proposal in
+                    ProposalListItemCondensedView(proposal: proposal) {
+                        Tracker.track(.prfVotesOpenDao)
+                        activeSheetManager.activeSheet = .daoInfo(proposal.dao)
+                    }
+                    .padding(.horizontal, 8)
+                    .onTapGesture {
+                        Tracker.track(.prfVotesOpenProposal)
+                        path.append(.vote(proposal))
+                    }
+                }
+            }
+        }
+        .padding(.bottom, 16)
+        .onAppear {
+            if votes.isEmpty {
                 data.getVotes()
             }
-        } else {
-            // list of votes here
         }
     }
 }
