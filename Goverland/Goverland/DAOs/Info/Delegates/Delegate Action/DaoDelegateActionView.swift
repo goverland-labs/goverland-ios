d2f616e0beb85571223bd1794826305128869718
Jenny Shalai <jenny.shalai@gmail.com>
2024-07-10 17:06:18 -0400

    added logic to increase/decrease the voting power in the list of delegates

--- Goverland/Goverland/DAOs/Info/Delegates/Delegate Action/DaoDelegateActionView.swift
+++ Goverland/Goverland/DAOs/Info/Delegates/Delegate Action/DaoDelegateActionView.swift
@@ -186,33 +186,36 @@ struct DaoDelegateActionView: View {
 
 fileprivate struct DelegateVotingPowerChoseView: View {
     
-    @State private var votingPower: Double = 43.0
-    private var 
+    private let votingPower: Double = 43.0
+    @State private var delegates: [Int: (User, Double)] = [0: (User.aaveChan, 11.0),
+                                                    1: (User.flipside, 32.0)]
+    private var sortedDelegates: [(key: Int, value: (User, Double))] {
+        delegates.sorted { $0.key < $1.key }
+    }
     
     
     var body: some View {
         VStack {
-            ForEach(0..<3) {_ in
+            ForEach(Array(sortedDelegates.enumerated()), id: \.1.key) { (index, element) in
                 HStack {
-                    IdentityView(user: User.aaveChan, onTap: nil)
+                    IdentityView(user: element.value.0, onTap: nil)
                     
                     Spacer()
                     
                     HStack(spacing: 0) {
                         Button(action: {
-                            
+                            decreaseVotingPower(forIndex: index)
                         }) {
                             Image(systemName: "minus")
                                 .frame(width: 40)
                                 .padding(.vertical)
                         }
                         
-                        Text(String(format: "%.0f", 2))
+                        Text(String(format: "%.0f", element.value.1))
                             .frame(width: 20)
                         
                         Button(action: {
-                            //increaseVotingPower(for: index)
-                            //voteButtonDisabled = totalPower == 0
+                            increaseVotingPower(forIndex: index)
                         }) {
                             Image(systemName: "plus")
                                 .frame(width: 40)
@@ -235,5 +238,33 @@ fileprivate struct DelegateVotingPowerChoseView: View {
                 )
             }
         }
+    }
+    
+    private func increaseVotingPower(forIndex index: Int) {
+        if let delegate = delegates[index] {
+            let newPower = delegate.1 + 1.0
+            let totalPower = getTotalVotingPower()
+            
+            if totalPower + (newPower - delegate.1) <= votingPower {
+                delegates[index] = (delegate.0, newPower)
+            } else {
+                // TODO: Handle exceeding limit (show an alert)
+                print("Exceeds total voting power limit!")
+            }
+        }
+    }
+    
+    private func decreaseVotingPower(forIndex index: Int) {
+        if let delegate = delegates[index] {
+            let newPower = delegate.1 - 1.0
+            
+            if newPower >= 0 {
+                delegates[index] = (delegate.0, newPower)
+            }
+        }
+    }
+    
+    private func getTotalVotingPower() -> Double {
+        return delegates.reduce(0.0) { $0 + $1.value.1 }
     }
 }
