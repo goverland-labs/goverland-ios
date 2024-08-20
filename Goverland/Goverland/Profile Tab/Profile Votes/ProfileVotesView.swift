1de33be78bceca91926ec96c0f6b03487d44273f
Andrey Scherbovich <andrey@goverland.xyz>
2024-08-20 12:16:59 +0200

    Revert "Remove grid layout for profile proposals"
    This reverts commit 35756727efabdf5ebf91c886f9df7303efbfb159.

--- Goverland/Goverland/Profile Tab/Profile Votes/ProfileVotesView.swift
+++ Goverland/Goverland/Profile Tab/Profile Votes/ProfileVotesView.swift
@@ -19,6 +19,14 @@ struct ProfileVotesView: View {
         dataSource.votedProposals ?? []
     }
     
+    var columns: [GridItem] {
+        if horizontalSizeClass == .regular {
+            return Array(repeating: .init(.flexible()), count: 2)
+        } else {
+            return Array(repeating: .init(.flexible()), count: 1)
+        }
+    }
+    
     var body: some View {
         VStack {
             HStack {
@@ -38,8 +46,9 @@ struct ProfileVotesView: View {
                     dataSource.refresh()
                 }
             } else if dataSource.isLoading && dataSource.votedProposals == nil { // initial loading
-                VStack {
-                    ForEach(0..<3, id: \.self) { _ in
+                LazyVGrid(columns: columns, spacing: 8) {
+                    let count = columns.count == 1 ? 3 : 4
+                    ForEach(0..<count, id: \.self) { _ in
                         ShimmerProposalListItemView()
                     }
                 }
@@ -51,8 +60,9 @@ struct ProfileVotesView: View {
                     .font(.bodyRegular)
                     .padding(16)
             } else {
-                VStack {
-                    ForEach(votedProposals.prefix(3)) { proposal in
+                LazyVGrid(columns: columns, spacing: 8) {
+                    let count = columns.count == 1 ? 3 : 4
+                    ForEach(votedProposals.prefix(count)) { proposal in
                         ProposalListItemView(proposal: proposal) {
                             Tracker.track(.prfVotesOpenDao)
                             activeSheetManager.activeSheet = .daoInfo(proposal.dao)
