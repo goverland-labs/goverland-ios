3b638123b851f73d5122c9105ccb26fa6d76b5a6
Andrey Scherbovich <andrey@goverland.xyz>
2024-08-31 15:17:49 +0200

    Move UserDelegationSplitViewModel initiation to a parent view

--- Goverland/Goverland/DAOs/Info/Delegates/User Delegation/DaoUserDelegationView.swift
+++ Goverland/Goverland/DAOs/Info/Delegates/User Delegation/DaoUserDelegationView.swift
@@ -11,25 +11,29 @@ import SwiftData
 
 struct DaoUserDelegationView: View {
     @StateObject private var dataSource: DaoUserDelegationDataSource
+    @Query private var profiles: [UserProfile]
 
     init(dao: Dao, delegate: User) {
         let dataSource = DaoUserDelegationDataSource(dao: dao, delegate: delegate)
         _dataSource = StateObject(wrappedValue: dataSource)
     }
 
+    private var appUser: User {
+        let profile = profiles.first(where: { $0.selected })!
+        return profile.user
+    }
+
     var body: some View {
-        Group {
+        VStack {
             if dataSource.isLoading {
-                VStack {
-                    ProgressView()
-                        .foregroundStyle(Color.textWhite20)
-                        .controlSize(.regular)
-                    Spacer()
-                }
+                ProgressView()
+                    .foregroundStyle(Color.textWhite20)
+                    .controlSize(.regular)
+                Spacer()
             } else if dataSource.failedToLoadInitialData {
                 RetryInitialLoadingView(dataSource: dataSource, message: "Sorry, we couldn’t load the delegation information")
             } else if dataSource.userDelegation != nil {
-                _DaoUserDelegationView(dataSource: dataSource)
+                _DaoUserDelegationView(appUser: appUser, dataSource: dataSource)
             }
         }
         .padding()
@@ -43,6 +47,7 @@ struct DaoUserDelegationView: View {
             }
         }
         .onAppear() {
+            // TODO: track
             if dataSource.userDelegation == nil {
                 dataSource.refresh()
             }
@@ -51,11 +56,21 @@ struct DaoUserDelegationView: View {
 }
 
 fileprivate struct _DaoUserDelegationView: View {
-    @ObservedObject var dataSource: DaoUserDelegationDataSource
+    let appUser: User
+    @ObservedObject private var dataSource: DaoUserDelegationDataSource
+    @StateObject private var splitViewModel: UserDelegationSplitViewModel
 
-    @Query private var profiles: [UserProfile]
     @Namespace var namespace
     @Environment(\.dismiss) private var dismiss
+
+    init(appUser: User, dataSource: DaoUserDelegationDataSource) {
+        self.appUser = appUser
+        _dataSource = ObservedObject(wrappedValue: dataSource)
+        let splitViewModel = UserDelegationSplitViewModel(owner: appUser,
+                                                          userDelegation: dataSource.userDelegation!,
+                                                          delegate: dataSource.delegate)
+        _splitViewModel = StateObject(wrappedValue: splitViewModel)
+    }
 
     private var userDelegation: DaoUserDelegation! {
         dataSource.userDelegation
@@ -69,125 +84,116 @@ fileprivate struct _DaoUserDelegationView: View {
         dataSource.delegate
     }
 
-    private var appUser: User {
-        let profile = profiles.first(where: { $0.selected })!
-        return profile.user
-    }
-
     var body: some View {
-        VStack {
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(spacing: 12) {
-                    HStack {
-                        Text("Selected wallet")
-                            .font(.bodyRegular)
-                            .foregroundStyle(Color.textWhite)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(spacing: 12) {
+                HStack {
+                    Text("Selected wallet")
+                        .font(.bodyRegular)
+                        .foregroundStyle(Color.textWhite)
 
-                        Spacer()
+                    Spacer()
 
-                        if let walletImage = WC_Manager.shared.sessionMeta?.walletImage {
-                            walletImage
-                                .frame(width: Avatar.Size.xs.profileImageSize, height: Avatar.Size.xs.profileImageSize)
-                                .scaledToFit()
-                                .clipShape(Circle())
-                        } else if let walletImageUrl = WC_Manager.shared.sessionMeta?.walletImageUrl {
-                            RoundPictureView(image: walletImageUrl, imageSize: Avatar.Size.s.profileImageSize)
-                        }
-
-                        IdentityView(user: appUser, size: .xs, font: .bodyRegular, onTap: nil)
+                    if let walletImage = WC_Manager.shared.sessionMeta?.walletImage {
+                        walletImage
+                            .frame(width: Avatar.Size.xs.profileImageSize, height: Avatar.Size.xs.profileImageSize)
+                            .scaledToFit()
+                            .clipShape(Circle())
+                    } else if let walletImageUrl = WC_Manager.shared.sessionMeta?.walletImageUrl {
+                        RoundPictureView(image: walletImageUrl, imageSize: Avatar.Size.s.profileImageSize)
                     }
 
-                    HStack {
-                        Text("Voting power")
-                            .font(.bodyRegular)
-                            .foregroundColor(.textWhite)
-                        Spacer()
+                    IdentityView(user: appUser, size: .xs, font: .bodyRegular, onTap: nil)
+                }
 
-                        HStack(spacing: 4) {
-                            Text(userDelegation.votingPower.power.description)
-                            Text(userDelegation.votingPower.symbol)
-                        }
+                HStack {
+                    Text("Voting power")
                         .font(.bodyRegular)
                         .foregroundColor(.textWhite)
+                    Spacer()
+
+                    HStack(spacing: 4) {
+                        Text(userDelegation.votingPower.power.description)
+                        Text(userDelegation.votingPower.symbol)
                     }
+                    .font(.bodyRegular)
+                    .foregroundColor(.textWhite)
+                }
 
-                    Divider()
-                        .background(Color.secondaryContainer)
-                        .padding(.vertical)
+                Divider()
+                    .background(Color.secondaryContainer)
+                    .padding(.vertical)
 
+                HStack {
+                    Text("Delegation scope")
+                        .font(.bodyRegular)
+                        .foregroundColor(.textWhite)
+                    Spacer()
                     HStack {
-                        Text("Delegation scope")
-                            .font(.bodyRegular)
-                            .foregroundColor(.textWhite)
-                        Spacer()
-                        HStack {
-                            RoundPictureView(image: dao.avatar(size: .xs), imageSize: Avatar.Size.xs.daoImageSize)
-                            Text(dao.name)
-                                .font(.bodySemibold)
-                                .foregroundStyle(Color.textWhite)
-                        }
+                        RoundPictureView(image: dao.avatar(size: .xs), imageSize: Avatar.Size.xs.daoImageSize)
+                        Text(dao.name)
+                            .font(.bodySemibold)
+                            .foregroundStyle(Color.textWhite)
                     }
+                }
 
+                HStack {
+                    Text("Network")
+                        .font(.bodyRegular)
+                        .foregroundColor(.textWhite)
+                    Spacer()
                     HStack {
-                        Text("Network")
-                            .font(.bodyRegular)
-                            .foregroundColor(.textWhite)
-                        Spacer()
-                        HStack {
-                            let chains = [userDelegation.chains.gnosis, userDelegation.chains.eth]
-                            ForEach(chains) { chain in
-                                ZStack {
-                                    if dataSource.selectedChain == chain {
-                                        Text(chain.name)
-                                            .foregroundColor(.clear)
-                                            .padding(.horizontal, 10)
-                                            .padding(.vertical, 6)
-                                            .background(Color.secondaryContainer)
-                                            .cornerRadius(20)
-                                            .matchedGeometryEffect(id: "network-background", in: namespace)
-                                    }
-
+                        let chains = [userDelegation.chains.gnosis, userDelegation.chains.eth]
+                        ForEach(chains) { chain in
+                            ZStack {
+                                if dataSource.selectedChain == chain {
                                     Text(chain.name)
-                                        .foregroundColor(Color.onSecondaryContainer)
+                                        .foregroundColor(.clear)
                                         .padding(.horizontal, 10)
                                         .padding(.vertical, 6)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 20)
-                                                .stroke(Color.secondaryContainer, lineWidth: 1)
-                                        )
+                                        .background(Color.secondaryContainer)
+                                        .cornerRadius(20)
+                                        .matchedGeometryEffect(id: "network-background", in: namespace)
                                 }
-                                .font(.caption2Semibold)
-                                .onTapGesture {
-                                    withAnimation(.spring(response: 0.5)) {
-                                        dataSource.selectedChain = chain
-                                    }
+
+                                Text(chain.name)
+                                    .foregroundColor(Color.onSecondaryContainer)
+                                    .padding(.horizontal, 10)
+                                    .padding(.vertical, 6)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .stroke(Color.secondaryContainer, lineWidth: 1)
+                                    )
+                            }
+                            .font(.caption2Semibold)
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.5)) {
+                                    dataSource.selectedChain = chain
                                 }
                             }
                         }
                     }
+                }
 
-                    UserDelegationSplitVotingPowerView(owner: appUser,
-                                                       userDelegation: userDelegation,
-                                                       delegate: delegate)
+                UserDelegationSplitVotingPowerView(viewModel: splitViewModel)
                     .padding(.bottom)
 
-                    SetDelegateExpirationView()
-                }
+                SetDelegateExpirationView()
             }
+        }
 
+        HStack {
             HStack {
-                HStack {
-                    SecondaryButton("Cancel") {
-                        dismiss()
-                    }
+                SecondaryButton("Cancel") {
+                    dismiss()
+                }
 
-                    PrimaryButton("Confirm") {
-                        Haptic.medium()
-                        Task {
-                            // TODO: api call to assign delegate
-                        }
-                        dismiss()
+                PrimaryButton("Confirm") {
+                    Haptic.medium()
+                    Task {
+                        // TODO: api call to assign delegate
                     }
+                    dismiss()
                 }
             }
         }
