285cd368b8f1db9aca63262796bb837607d2f591
Jenny Shalai <jenny.shalai@gmail.com>
2024-09-12 11:52:24 -0400

    reload content for delegate activity if fail to load, shimmering while loading

--- Goverland/Goverland/DAOs/Info/Delegates/Delegate Profile/DaoDelegateProfileActivityView.swift
+++ Goverland/Goverland/DAOs/Info/Delegates/Delegate Profile/DaoDelegateProfileActivityView.swift
@@ -18,17 +18,28 @@ struct DaoDelegateProfileActivityView: View {
     }
     
     var body: some View {
-        List(0..<dataSource.votes.count, id: \.self) { i in
-            ProposalListItemView(proposal: dataSource.votes[i], isSelected: false, isRead: false) {}
-                .listRowSeparator(.hidden)
-                .listRowInsets(Constants.listInsets)
-                .listRowBackground(Color.clear)
-        }
-        .listStyle(.plain)
-        .scrollIndicators(.hidden)
-        .onAppear() {
-            if dataSource.votes.isEmpty {
+        if dataSource.failedToLoadInitialData {
+            RefreshIcon {
                 dataSource.refresh()
+            }
+        } else if dataSource.isLoading && dataSource.votes.isEmpty {
+            ForEach(0..<1) { _ in
+                ShimmerProposalListItemView()
+                    .padding(.horizontal, Constants.horizontalPadding)
+            }
+        } else {
+            List(0..<dataSource.votes.count, id: \.self) { i in
+                ProposalListItemView(proposal: dataSource.votes[i], isSelected: false, isRead: false) {}
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(Constants.listInsets)
+                    .listRowBackground(Color.clear)
+            }
+            .listStyle(.plain)
+            .scrollIndicators(.hidden)
+            .onAppear() {
+                if dataSource.votes.isEmpty {
+                    dataSource.refresh()
+                }
             }
         }
     }
