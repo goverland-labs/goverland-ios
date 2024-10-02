5e22b6813d4f15c35ecb5dcc4aa8367da174ff23
Andrey Scherbovich <andrey@goverland.xyz>
2024-10-02 12:00:46 +0200

    add more logging

--- Goverland/Goverland/Authentication/Coinbase Wallet/CoinbaseWalletManager.swift
+++ Goverland/Goverland/Authentication/Coinbase Wallet/CoinbaseWalletManager.swift
@@ -14,6 +14,7 @@ class CoinbaseWalletManager {
     static let shared = CoinbaseWalletManager()
 
     private init() {
+        logInfo("[App] configuring Coinbase SDK")
         configure()
         getStoredAccount()
     }
